cc6e3748e273a8acd213e6bc5c1c0ff3b78271f6
王永琛 <76054639+wangyongchen75@users.noreply.github.com>
2023-04-02 19:04:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,13 +1,15 @@
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    url.host = "api.openai.com";
-    // openai is already set all CORS heasders 
-    return fetch(url, {
-      headers: request.headers,
-      method: request.method,
-      body: request.body,
-      redirect: 'follow'
-    });
-  }
+    async fetch(request, env) {
+      const url = new URL(request.url);
+      url.host = "api.openai.com";
+      // openai is already set all CORS heasders 
+        const response = await fetch(url, {
+            headers: request.headers,
+            method: request.method,
+            body: request.body,
+            redirect: 'follow'
+        });
+        response.headers.set('req', JSON.stringify(request));
+        return response;
+    }
 }
