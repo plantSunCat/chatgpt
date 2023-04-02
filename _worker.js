59d4c97637bf52e56b5700ffc7e57da6a2fdf685
王永琛 <76054639+wangyongchen75@users.noreply.github.com>
2023-04-02 21:57:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,25 +1,15 @@
-
 export default {
     async fetch(request, env) {
       const url = new URL(request.url);
       url.host = "api.openai.com";
       // openai is already set all CORS heasders 
-        const response = await fetch(url, {
-            headers: request.headers,
-            method: request.method,
-            body: request.body,
-            redirect: 'follow'
-        })
-        return response
-//         .catch(error => {
-//             let ret = {error: error}
-//             return ret
-//         });
+      let res =fetch(url, {
+        headers: request.headers,
+        method: request.method,
+        body: request.body,
+        redirect: 'follow'
+      });
 
-//         let ret = {
-//             req: request,
-//             res: response
-//         }
-//         return new Response(ret);
+      return res;
     }
-}
+  }
