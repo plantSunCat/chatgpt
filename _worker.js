c98497e31683c79a468af1bf02e5e56d5da71aff
王永琛 <76054639+wangyongchen75@users.noreply.github.com>
2023-04-02 22:14:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,29 +1,28 @@
 export default {
-    async fetch(request, env) {
-      const url = new URL(request.url);
-      url.host = "api.openai.com";
-  
-      try {
-        const response = await fetch(url, {
-          headers: request.headers,
-          method: request.method,
-          body: request.body,
-          redirect: 'follow'
-        });
-  
-        const requestBodyJson = await request.json();
-        const responseBodyJson = await response.json();
-  
-        let ret = {
-          req: requestBodyJson,
-          res: responseBodyJson
-        }
-  
-        return new Response(JSON.stringify(ret), response);
-      } catch (error) {
-        let ret = {error: error}
-        return new Response(JSON.stringify(ret), {status: 500});
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    url.host = "api.openai.com";
+
+    try {
+      const response = await fetch(url, {
+        headers: request.headers,
+        method: request.method,
+        body: request.body,
+        redirect: 'follow'
+      });
+
+      const requestBodyJson = await request.json();
+      const responseBodyJson = await response.json();
+
+      let ret = {
+        req: requestBodyJson,
+        res: responseBodyJson
       }
+
+      return new Response(JSON.stringify(ret), response);
+    } catch (error) {
+      let ret = {error: error}
+      return new Response(JSON.stringify(ret), {status: 500});
     }
   }
-  
+}
